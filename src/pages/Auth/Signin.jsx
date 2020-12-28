9beb99e97e64e09e06b05e8ebfa60bfb8933463b
조신영 <sycho99@svcorps.dev>
2020-12-28 13:48:14 +0900

    Signin 페이지 UI 추가

--- src/pages/Auth/Signin.jsx
+++ src/pages/Auth/Signin.jsx
@@ -1,9 +1,88 @@
 import React from "react";
-import { Box, Container } from "@material-ui/core";
+import { makeStyles, Avatar, Box, Container, TextField, InputAdornment } from "@material-ui/core";
+import { Typography, Button } from "components/materialui";
+import { PersonOutline, LockOutlined } from "@material-ui/icons";
 import { useForm } from "react-hook-form";
 
-export const Signin = ({}) => {
-  const {} = useForm();
+const useStyles = makeStyles({
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  },
+  logo: {
+    width: "20rem",
+    height: "7rem",
+    marginBottom: "2rem",
+    "& img": {
+      objectFit: "contain",
+    },
+  },
 
-  return <Container>hi</Container>;
+  input: {
+    width: "22rem",
+    marginTop: "0.5rem",
+  },
+});
+
+export const Signin = ({}) => {
+  const classes = useStyles();
+  const { register, errors, handleSubmit } = useForm();
+
+  function handleSignin(data) {
+    console.log(data);
+  }
+
+  return (
+    <Container maxWidth="md">
+      <Box className={classes.wrapper}>
+        <Avatar className={classes.logo} variant="square" src="/image/logo_color.png" />
+
+        <TextField
+          className={classes.input}
+          variant="outlined"
+          name="email"
+          placeholder="Email"
+          inputRef={register({ required: true })}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <PersonOutline />
+              </InputAdornment>
+            ),
+          }}
+          error={errors?.email}
+        />
+        <TextField
+          className={classes.input}
+          variant="outlined"
+          name="password"
+          placeholder="Password"
+          inputRef={register({ required: true })}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <LockOutlined />
+              </InputAdornment>
+            ),
+          }}
+          error={errors?.password}
+        />
+
+        <Box display="flex" alignItems="center" mt={2} mb={6}>
+          <Button variant="text">아이디/패스워드 찾기</Button>
+          &#x0007C;
+          <Button variant="text">회원가입</Button>
+        </Box>
+
+        <Button variant="contained" color="primary" px={18} py={2} onClick={handleSubmit(handleSignin)}>
+          <Typography variant="h6" fontWeight={"700"}>
+            로그인
+          </Typography>
+        </Button>
+      </Box>
+    </Container>
+  );
 };
