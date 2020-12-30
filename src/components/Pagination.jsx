9f1091f5f1f1c243a3993260b21d12fb498d94a5
조신영 <sycho99@svcorps.dev>
2020-12-30 16:50:05 +0900

    Pagination 컴포넌트 추가

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Box } from "@material-ui/core";
+import { Pagination, PaginationItem } from "@material-ui/lab";
+
+const MyPagination = ({ page, setPage }) => {
+  return (
+    <Box position="absolute" display="flex" justifyContent="center" alignItems="center" width="100%" height="0px">
+      <Pagination
+        color="primary"
+        count={10}
+        page={page}
+        onChange={(e, v) => setPage(v)}
+        shape="rounded"
+        renderItem={(item) => <PaginationItem name="page" {...item} />}
+      />
+    </Box>
+  );
+};
+export { MyPagination as Pagination };
