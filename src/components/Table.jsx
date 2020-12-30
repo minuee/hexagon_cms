03e24badc819139dc59b425bf870954f6ec82e31
조신영 <sycho99@svcorps.dev>
2020-12-30 15:22:08 +0900

    Material-table 컴포넌트 추가

---
+++ src/components/Table.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { makeStyles, Box } from "@material-ui/core";
+import MaterialTable from "material-table";
+
+const useStyles = makeStyles((theme) => ({
+  table_wrapper: {
+    "& *": {
+      textAlign: "center",
+    },
+    "& tr": {
+      fontFamily: theme.typography.fontFamily,
+    },
+    "& th": {
+      background: theme.palette.grey[300],
+    },
+  },
+}));
+
+export const Table = ({ columns, data, onRowClick = () => {} }) => {
+  const classes = useStyles();
+  return (
+    <Box className={classes.table_wrapper}>
+      <MaterialTable
+        data={data}
+        columns={columns}
+        onRowClick={(e, row) => onRowClick(row)}
+        options={{
+          toolbar: false,
+          search: false,
+          draggable: false,
+          sorting: false,
+
+          paging: false,
+        }}
+      />
+    </Box>
+  );
+};
