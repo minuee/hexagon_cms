d38709a0d09bd11517260c2341b19644d806366d
조신영 <sycho99@svcorps.dev>
2021-03-05 11:10:18 +0900

    영업사원 관리회원 API로직 수정

--- src/pages/Salesman/Member/List.jsx
+++ src/pages/Salesman/Member/List.jsx
@@ -6,25 +6,15 @@ import { apiObject } from "api";
 import qs from "query-string";
 
 import { Grid, Box, makeStyles } from "@material-ui/core";
-import { ArrowDropDown, ArrowDropUp } from "@material-ui/icons";
-import { Typography, Button } from "components/materialui";
+import { Typography } from "components/materialui";
 import { ColumnTable, Pagination, SearchBox } from "components";
 
 const useStyles = makeStyles((theme) => ({
-  header_buttons: {
-    display: "inline-flex",
-    alignItems: "center",
-
-    "& > *": {
-      marginleft: theme.spacing(1),
-    },
-  },
   table_footer: {
     position: "relative",
     display: "flex",
     alignItems: "center",
     justifyContent: "flex-end",
-    // justifyContent: "space-between",
 
     "& >:first-child, >:last-child": {
       background: "#fff",
@@ -36,40 +26,12 @@ const member_list_columns = [
   { title: "이름", field: "name", width: 160 },
   { title: "코드값", field: "special_code", width: 100 },
   { title: "휴대폰번호", field: "phone", width: 160 },
-  // { title: "이메일주소", field: "email" },
   {
     title: "구매액",
     render: ({ total_amount }) => `${price(total_amount) || 0}원`,
     cellStyle: { textAlign: "right" },
   },
-  // {
-  //   title: "리워드액",
-  //   render: ({ reward_point }) => `${price(reward_point) || 0}원`,
-  //   cellStyle: { textAlign: "right" },
-  // },
   { title: "등급", field: "grade_name", width: 100 },
-  // {
-  //   title: "비고",
-  //   render: ({ approval, agent_code }) => (approval ? `영업사원코드:  ${agent_code}` : "회원가입 미승인"),
-  // },
-];
-const header_button_list = [
-  {
-    label: "이름순",
-    value: "uname",
-  },
-  {
-    label: "가입일자순",
-    value: "reg",
-  },
-  {
-    label: "번호순",
-    value: "no",
-  },
-  {
-    label: "구매액순",
-    value: "order",
-  },
 ];
 
 export const ManageMemberList = ({ location }) => {
@@ -89,13 +51,6 @@ export const ManageMemberList = ({ location }) => {
   }
 
   function handleQueryChange(q, v) {
-    if (q == "sort_item") {
-      if (query[q] == v) {
-        query.sort_type = query?.sort_type === "DESC" ? "ASC" : "DESC";
-      } else {
-        query.sort_type = "DESC";
-      }
-    }
     if (q !== "page") {
       query.page = 1;
     }
@@ -110,22 +65,9 @@ export const ManageMemberList = ({ location }) => {
 
   return (
     <Box>
-      <Grid container justify="space-between" alignItems="center">
-        <Typography display="inline" variant="h5" fontWeight="500">
-          회원 목록
-        </Typography>
-
-        <Box className={classes.header_buttons}>
-          {header_button_list.map((item, index) => (
-            <Button variant="text" onClick={() => handleQueryChange("sort_item", item.value)} key={index}>
-              <Typography fontWeight={query.sort_item === item.value ? "700" : undefined}>{item.label}</Typography>
-              {query.sort_item === item.value && (
-                <>{query.sort_type === "DESC" ? <ArrowDropDown /> : <ArrowDropUp />}</>
-              )}
-            </Button>
-          ))}
-        </Box>
-      </Grid>
+      <Typography variant="h5" fontWeight="500">
+        회원 목록
+      </Typography>
 
       <Box mt={2} mb={3}>
         <ColumnTable
