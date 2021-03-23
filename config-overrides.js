597bb03e0e0ca5789d80c2c48d14df1395df1f1c
조신영 <sycho99@svcorps.dev>
2021-03-23 14:03:59 +0900

    절대경로설정 수정

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,10 @@
+const path = require("path");
+
 module.exports = {
   webpack: function (config, env) {
     let is_prod = env === "production";
+
+    config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, "src")];
     config.output.filename = is_prod ? "[name].[contenthash:8].js" : "[name].[hash:8].js";
     return config;
   },
